568522e7e190941b041e564113450a746f5bd9ea
Gaurav Desai <gauravdesai111@gmail.com>
2021-05-13 00:56:42 +0530

    Added Timerotating logger

--- API_wrapper.py
+++ API_wrapper.py
@@ -10,18 +10,28 @@ import platform
 import os
 from urllib.error import HTTPError, URLError
 import config
+from logging.handlers import TimedRotatingFileHandler
 
 
 class COWINConnection:
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s')
-    logging.root.setLevel(logging.WARNING)
+    log_file_name = "API_WRAPPER.log"
+    log_format = '%(asctime)s %(levelname)s:%(message)s'
+    log_handler = TimedRotatingFileHandler(log_file_name, when="midnight", interval=1)
+
+    formatter = logging.Formatter(log_format)
+    log_handler.setFormatter(formatter)
+    
+    log_handler.suffix = "%Y%m%d"
+    logger = logging.getLogger("COWINConnection")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_handler)
 
     @staticmethod
     def generate_url(api_name, api_type):
         assert api_type in config.COWIN_API
         api_url = config.COWIN_API[api_type][api_name]
         full_url = config.PROD_SERVER + api_url
-        logging.info(f'full_url={full_url}')
+        COWINConnection.logger.info(f'full_url={full_url}')
         return full_url
 
     @staticmethod
@@ -39,25 +49,25 @@ class COWINConnection:
                 response = COWINConnection.fetch_data_using_GET(data_input, url, headers)
 
             return_code = response.getcode()
-            logging.info(f"Return code = {return_code}")
+            COWINConnection.logger.info(f"Return code = {return_code}")
             if return_code == 200:  # OK
                 data = response.read().decode('utf-8')
                 data = ast.literal_eval(data)
-                logging.info(f"data={(data)}")
+                COWINConnection.logger.info(f"data={(data)}")
                 return data
             else:
-                logging.warning(f'For url {url}, server returned code {return_code}. No data retrieved.')
+                COWINConnection.logger.warning(f'For url {url}, server returned code {return_code}. No data retrieved.')
         except HTTPError as e:
-            logging.warning(f'HTTPError. Error code={e.errno}, Reason:{e.reason}')
+            COWINConnection.logger.warning(f'HTTPError. Error code={e.errno}, Reason:{e.reason}')
         except URLError as e:
-            logging.warning(f'URLError:Unable to reach to server. Error code={e.errno}, Reason:{e.reason}')
+            COWINConnection.logger.warning(f'URLError:Unable to reach to server. Error code={e.errno}, Reason:{e.reason}')
 
         return None
 
     @staticmethod
     def fetch_data_using_GET(data_input, url, headers):
         url = (url + "?" + urllib.parse.urlencode(data_input)) if data_input else url
-        logging.info(f'GET request data={url}')
+        COWINConnection.logger.info(f'GET request data={url}')
         request = urllib.request.Request(url, data=None, headers=headers)
         response = urllib.request.urlopen(request)
         return response
@@ -66,7 +76,7 @@ class COWINConnection:
     def fetch_data_using_POST(data_input, url, headers):
         data = json.dumps(data_input)
         data = data.encode('ascii')
-        logging.debug(f"After ascii encode data={data}")
+        COWINConnection.logger.debug(f"After ascii encode data={data}")
         headers['content-type'] = 'application/json'
         request = urllib.request.Request(url, data,
                                          headers=headers)
@@ -83,7 +93,7 @@ class COWINConnection:
 
     @staticmethod
     def get_formatted_date(date_obj):
-        logging.debug(f'date_obj datatype={type(date_obj)}')
+        COWINConnection.logger.debug(f'date_obj datatype={type(date_obj)}')
         if date_obj:
             if type(date_obj) == str:  # already formatted
                 return date_obj
@@ -118,10 +128,9 @@ class COWINConnection:
         for_date = COWINConnection.get_formatted_date(for_date)
         return self.get_data_from_url('GET_SESSIONS_BY_DISTRICT_7_DAYS', 'APPOINTMENT_AVAILABILITY_API',
                                       data_input={'district_id': str(district_id), 'date': for_date})
-    
+
     def generate_otp(self, mobile):
         return self.get_data_from_url('GENERATE_OTP', 'USER_AUTHENTICATION_API', data_input={'mobile': mobile})
-
 
     @staticmethod
     def print_centers(centers):
@@ -152,15 +161,15 @@ class COWINConnection:
                     if push_notification_required:
                         self.push_notification()
                     if otp_required:
-                        logging.info(f'Generating otp for mobile:{mobile}')
+                        COWINConnection.logger.info(f'Generating otp for mobile:{mobile}')
                         otp = self.generate_otp(mobile=mobile)
                     if stop_if_found:
-                        logging.warning(f'stop_if_found is set to {stop_if_found}, hence stopping the program')
+                        COWINConnection.logger.warning(f'stop_if_found is set to {stop_if_found}, hence stopping the program')
                         break
-                logging.info(f'Sleeping for {SLEEP_SECONDS} seconds')
+                COWINConnection.logger.info(f'Sleeping for {SLEEP_SECONDS} seconds')
                 time.sleep(SLEEP_SECONDS)
             except Exception as e:
-                logging.error(f'Exception while executing {str(func)}.\n{e}')
+                COWINConnection.logger.error(f'Exception while executing {str(func)}.\n{e}')
 
     @staticmethod
     def push_notification():
@@ -172,7 +181,7 @@ class COWINConnection:
             osascript -e 'display notification "{message}" with title "{title}"'
             '''
         else:
-            logging.warning(f'Push notifications not supported for this system ({platform.system()})')
+            COWINConnection.logger.warning(f'Push notifications not supported for this system ({platform.system()})')
             return
 
         os.system(command)
--- appointment_monitor.py
+++ appointment_monitor.py
@@ -1,22 +1,34 @@
 import logging
 import sys
 from datetime import date
+from logging.handlers import TimedRotatingFileHandler
 
 from API_wrapper import COWINConnection
 
 
 class AppointmentMonitor:
+    log_file_name = "APPOINTMENT_MONITOR.log"
+    log_format = '%(asctime)s %(levelname)s:%(message)s'
+    log_handler = TimedRotatingFileHandler(log_file_name, when="midnight", interval=1)
+    
+    formatter = logging.Formatter(log_format)
+    log_handler.setFormatter(formatter)
+
+    log_handler.suffix = "%Y%m%d"
+    logger = logging.getLogger("AppointmentMonitor")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_handler)
 
     @staticmethod
     def find_appointment_for_age_by_district(wrapper_conn, district_id=None, min_age_limit=None, from_date=None):
         returned_data = wrapper_conn.find_appointment_by_calendar_district(district_id, from_date)
 
         if not returned_data or "centers" not in returned_data.keys():
-            logging.warning("API did not return any data. continuing to check")
+            AppointmentMonitor.logger.warning("API did not return any data. continuing to check")
             return 0
 
         centers = returned_data["centers"]
-        logging.info(f"Received {len(centers)} centers before filter")
+        AppointmentMonitor.logger.info(f"Received {len(centers)} centers before filter")
 
         eligible_centers = []
         for center in centers:
@@ -24,7 +36,7 @@ class AppointmentMonitor:
             eligible_sessions = []
             for cur_ses in cur_sessions:
                 if cur_ses['min_age_limit'] <= int(min_age_limit) and cur_ses['available_capacity'] > 0:
-                    logging.info(f"Eligible session {cur_ses} at center {center}")
+                    AppointmentMonitor.logger.info(f"Eligible session {cur_ses} at center {center}")
                     eligible_sessions.append(cur_ses)
 
             if len(eligible_sessions) > 0:
@@ -32,7 +44,7 @@ class AppointmentMonitor:
                 temp_center['sessions'] = eligible_sessions
                 eligible_centers.append(temp_center)
 
-        logging.info(f'After filtering found {len(eligible_centers)} eligible and available centers')
+        AppointmentMonitor.logger.info(f'After filtering found {len(eligible_centers)} eligible and available centers')
         wrapper_conn.print_centers(eligible_centers)
         return eligible_centers
 
